b328425ab71488bc74b3260f5d947cf63ef581e9
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-06-23 18:31:32 +0900

    event listeners added, drum kit is working

--- 01-JavaScript-Drum-Kit/index.js
+++ 01-JavaScript-Drum-Kit/index.js
@@ -4,10 +4,22 @@
 // change the class of the div to reflect the keypress
 // remove the keypress after the transition phase ends
 
+// listen for the keydown event, then play the audio associated with it, add the 'button-pressed' class
 window.addEventListener('keydown', function (event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const key = document.querySelector(`.button[data-key="${event.keyCode}"`);
   if(!audio) return; // if no audio for that key, will stop the function
   audio.currentTime = 0; // this will rewind the audio so it can play faster
   audio.play();
+  key.classList.add('button-pressed');
 });
+
+// remove the 'button-pressed' class after the transform ends
+function removeTransition(event) {
+  if(event.propertyName !== 'transform') return; // skip it if it's not a transform
+  this.classList.remove('button-pressed');
+};
+
+// listen for the transitionend action to trigger the function to remove the 'button-pressed class'
+const keys = document.querySelectorAll('.button');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
