856d767784a805d000ade72aae14f70c9d874f63
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-06-23 17:51:54 +0900

    audio tags written, div data keys assigned

---
+++ 01-JavaScript-Drum-Kit/index.js
@@ -0,0 +1,6 @@
+// add event listener for key press
+// record which key was pressed
+// call a function to play the appropriate audio
+// change the class of the div to reflect the keypress
+// remove the keypress after the transition phase ends
+
