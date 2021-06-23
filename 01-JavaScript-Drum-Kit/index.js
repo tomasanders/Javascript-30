f8efc626121d05174ed2aaabc84a81502fe366b4
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-06-23 18:11:12 +0900

    added javascript to play the audio when the key is pressed

--- 01-JavaScript-Drum-Kit/index.js
+++ 01-JavaScript-Drum-Kit/index.js
@@ -4,3 +4,10 @@
 // change the class of the div to reflect the keypress
 // remove the keypress after the transition phase ends
 
+window.addEventListener('keydown', function (event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.button[data-key="${event.keyCode}"`);
+  if(!audio) return; // if no audio for that key, will stop the function
+  audio.currentTime = 0; // this will rewind the audio so it can play faster
+  audio.play();
+});
